33d5b8239cc2ac90e69b6a2127380b0097aff1a5
Michal Sanda <sandamichal94@gmail.com>
2025-02-08 15:29:05 +0100

    QSqlRelationalTableModel for displaying the foreign key zakazka title

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QFileDialog, QTableView, QHBoxLayout, QAbstractItemView, QLabel, QLineEdit, QVBoxLayout, QMessageBox, QMenu, QFormLayout, QSpinBox, QDialogButtonBox, QDialog, QStyledItemDelegate
-from PySide6.QtSql import QSqlDatabase, QSqlTableModel
+from PySide6.QtSql import QSqlDatabase, QSqlTableModel, QSqlRelationalTableModel, QSqlRelationalDelegate, QSqlRelation
 from PySide6.QtCore import QSortFilterProxyModel, Qt
 from PySide6.QtGui import QBrush
 import sys
@@ -194,16 +194,23 @@ class PDFImporterApp(QWidget):
         self.zakazka_table.setColumnWidth(2, 250)
 
         # Polozka model
-        self.polozka_model = QSqlTableModel(self, db)
+        self.polozka_model = QSqlRelationalTableModel(self, db)
         self.polozka_model.setTable("položka")
+
+        self.polozka_model.setRelation(4, QSqlRelation("zakázka", "id", "title"))
+
         self.polozka_model.select()  # Load the data
+
+        # Set headers
         self.polozka_model.setHeaderData(1, Qt.Orientation.Horizontal, "Číslo")
         self.polozka_model.setHeaderData(2, Qt.Orientation.Horizontal, "Název")
         self.polozka_model.setHeaderData(3, Qt.Orientation.Horizontal, "Ks")
         self.polozka_model.setHeaderData(4, Qt.Orientation.Horizontal, "Zakázka")
         self.polozka_model.setHeaderData(5, Qt.Orientation.Horizontal, "Výkres")
 
+        # Assign to table
         self.polozka_table.setModel(self.polozka_model)
+        self.polozka_table.setItemDelegate(QSqlRelationalDelegate(self.polozka_table))  # Enables dropdown for relations
         self.polozka_table.hideColumn(0)
         self.polozka_table.hideColumn(6)
         self.polozka_table.setSortingEnabled(True)
@@ -213,14 +220,14 @@ class PDFImporterApp(QWidget):
         self.polozka_table.setColumnWidth(1, 70)
         self.polozka_table.setColumnWidth(2, 350)
         self.polozka_table.setColumnWidth(3, 30)
-        self.polozka_table.setColumnWidth(4, 80)
+        self.polozka_table.setColumnWidth(4, 150)
         self.polozka_table.setColumnWidth(5, 80)
 
         # Proxy model for filtering polozka
         self.polozka_filter_model = QSortFilterProxyModel(self)
         self.polozka_filter_model.setSourceModel(self.polozka_model)
         self.polozka_filter_model.setFilterCaseSensitivity(Qt.CaseInsensitive)
-        self.polozka_filter_model.setFilterKeyColumn(4)  # Filter based on the zakazka_number
+        self.polozka_filter_model.setFilterKeyColumn(4)  # Filter based on the zakazka_number (4th column in the polozka table)
         self.polozka_filter_model.setFilterFixedString("")  # No filter initially
 
         self.polozka_table.setModel(self.polozka_filter_model)
